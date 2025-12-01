36535dd071fa3627d80e18957f154034f587cffb
KuanYan666 <kuanyan666@gmail.com>
2025-12-01 18:56:23 +0800

    created mk5n folder and renamed swerve modules

---
+++ src/main/java/lib/ironpulse/swerve/mk5n/SwerveMK5NConfig.java
@@ -0,0 +1,15 @@
+package lib.ironpulse.swerve.mk5n;
+
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Frequency;
+import lib.ironpulse.swerve.SwerveConfig;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+public class SwerveMK5NConfig extends SwerveConfig {
+    public Current driveStatorCurrentLimit;
+    public Current steerStatorCurrentLimit;
+    public Frequency odometryFrequency;
+    public String canivoreCanBusName;
+    public int pigeonId;
+}
