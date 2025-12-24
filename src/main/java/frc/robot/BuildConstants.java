ef0b236be601e533ded644f00a95d5a5c09851d1
Mai961 <michael96mai@gmail.com>
2025-12-24 18:13:07 +0800

    [feat] working swerve

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "lib-ironpulse";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 15;
-  public static final String GIT_SHA = "d3113b8969afaf6ced2375785da0cb35ce7fdc9f";
-  public static final String GIT_DATE = "2025-12-18 20:11:57 CST";
+  public static final int GIT_REVISION = 16;
+  public static final String GIT_SHA = "00e7d03a6918016d5221dfe1f065620db03a0c65";
+  public static final String GIT_DATE = "2025-12-19 17:47:00 CST";
   public static final String GIT_BRANCH = "swerve";
-  public static final String BUILD_DATE = "2025-12-19 17:37:06 CST";
-  public static final long BUILD_UNIX_TIME = 1766137026007L;
+  public static final String BUILD_DATE = "2025-12-24 17:41:57 CST";
+  public static final long BUILD_UNIX_TIME = 1766569317437L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
