aeac9b1ea5d733d6b24511893c647898dd1d85cb
Mai961 <michael96mai@gmail.com>
2026-01-12 22:31:43 +0100

    [fix][important]wheel speed incorrect in sim

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "lib-ironpulse";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 70;
-    public static final String GIT_SHA = "738ddfec31282bb72145bebae81db6b4c90ca3ff";
-    public static final String GIT_DATE = "2026-01-13 02:47:35 CST";
+    public static final int GIT_REVISION = 71;
+    public static final String GIT_SHA = "002f6dd541ff0c723f3bcb9063e4d17a4205ec68";
+    public static final String GIT_DATE = "2026-01-13 05:06:22 CST";
     public static final String GIT_BRANCH = "2026Dev/ShootingRelated";
-    public static final String BUILD_DATE = "2026-01-13 04:58:28 CST";
-    public static final long BUILD_UNIX_TIME = 1768251508855L;
+    public static final String BUILD_DATE = "2026-01-13 05:28:30 CST";
+    public static final long BUILD_UNIX_TIME = 1768253310108L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
