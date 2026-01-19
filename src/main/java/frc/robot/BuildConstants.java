e9a68eaea45e12e0221e203bdf80170decf2d63e
Mai961 <michael96mai@gmail.com>
2026-01-19 17:03:17 +0100

    [feat]update to 2026.2.1

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
-  public static final String MAVEN_NAME = "main-2026";
+  public static final String MAVEN_NAME = "lib-ironpulse";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 85;
-  public static final String GIT_SHA = "99559a7c538cc11101fe7428cea3cee55ad4a7b5";
-  public static final String GIT_DATE = "2026-01-19 13:23:35 CST";
+  public static final int GIT_REVISION = 87;
+  public static final String GIT_SHA = "c53d781d5fe85c5d427fc7dc5a1996eb916eaa9b";
+  public static final String GIT_DATE = "2026-01-19 23:43:08 CST";
   public static final String GIT_BRANCH = "2026Dev/Shooter";
-  public static final String BUILD_DATE = "2026-01-19 19:00:32 CST";
-  public static final long BUILD_UNIX_TIME = 1768820432883L;
+  public static final String BUILD_DATE = "2026-01-20 00:02:32 CST";
+  public static final long BUILD_UNIX_TIME = 1768838552205L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
