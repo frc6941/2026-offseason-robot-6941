93ab733540983d6df8c964e63d8f717a342cd2b8
Mai961 <michael96mai@gmail.com>
2026-02-10 16:16:23 +0800

    [fix]canid

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -38,6 +38,8 @@ import lib.ironpulse.subsystem.position.PositionMotorSubsystem;
 import lib.ironpulse.subsystem.velocity.VelocityMotorSubsystem;
 import lib.ironpulse.swerve.Swerve;
 import lib.ironpulse.swerve.SwerveCommands;
+import lib.ironpulse.swerve.mk5n.ImuIOPigeon;
+import lib.ironpulse.swerve.mk5n.SwerveModuleIOMK5N;
 import lib.ironpulse.swerve.sim.ImuIOSim;
 import lib.ironpulse.swerve.sim.SwerveModuleIOSimpleSim;
 import lib.ironpulse.utils.AllianceFlipUtil;
@@ -68,22 +70,22 @@ public class RobotContainer {
     public RobotContainer() {
 
         if (RobotBase.isReal()) {
-            //     swerve =
-            //             new Swerve(
-            //                     SwerveMK5Config.kRealConfig,
-            //                     new ImuIOPigeon(SwerveMK5Config.kRealConfig),
-            //                     new SwerveModuleIOMK5N(SwerveMK5Config.kRealConfig, 0),
-            //                     new SwerveModuleIOMK5N(SwerveMK5Config.kRealConfig, 1),
-            //                     new SwerveModuleIOMK5N(SwerveMK5Config.kRealConfig, 2),
-            //                     new SwerveModuleIOMK5N(SwerveMK5Config.kRealConfig, 3));
-            swerve =
-                    new Swerve(
-                            SwerveMK5Config.kSimConfig,
-                            new ImuIOSim(),
-                            new SwerveModuleIOSimpleSim(SwerveMK5Config.kSimConfig, 0),
-                            new SwerveModuleIOSimpleSim(SwerveMK5Config.kSimConfig, 1),
-                            new SwerveModuleIOSimpleSim(SwerveMK5Config.kSimConfig, 2),
-                            new SwerveModuleIOSimpleSim(SwerveMK5Config.kSimConfig, 3));
+                swerve =
+                        new Swerve(
+                                SwerveMK5Config.kRealConfig,
+                                new ImuIOPigeon(SwerveMK5Config.kRealConfig),
+                                new SwerveModuleIOMK5N(SwerveMK5Config.kRealConfig, 0),
+                                new SwerveModuleIOMK5N(SwerveMK5Config.kRealConfig, 1),
+                                new SwerveModuleIOMK5N(SwerveMK5Config.kRealConfig, 2),
+                                new SwerveModuleIOMK5N(SwerveMK5Config.kRealConfig, 3));
+        //     swerve =
+        //             new Swerve(
+        //                     SwerveMK5Config.kSimConfig,
+        //                     new ImuIOSim(),
+        //                     new SwerveModuleIOSimpleSim(SwerveMK5Config.kSimConfig, 0),
+        //                     new SwerveModuleIOSimpleSim(SwerveMK5Config.kSimConfig, 1),
+        //                     new SwerveModuleIOSimpleSim(SwerveMK5Config.kSimConfig, 2),
+        //                     new SwerveModuleIOSimpleSim(SwerveMK5Config.kSimConfig, 3));
             //     limelightSubsystem =
             //             new LimelightSubsystem(
             //                     RobotConstants.LimelightConstants.limelightSubsystemConfig,
@@ -242,7 +244,7 @@ public class RobotContainer {
                         RobotStateRecorder::getPoseDriverRobotCurrent,
                         MetersPerSecond.of(0.04),
                         DegreesPerSecond.of(3.0)));
-        turret.setDefaultCommand(turret.runTurretTargetLoop());
+        //turret.setDefaultCommand(turret.runTurretTargetLoop());
     }
 
     public void robotPeriodic() {
