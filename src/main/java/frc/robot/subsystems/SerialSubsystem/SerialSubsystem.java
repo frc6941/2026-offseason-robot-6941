4f2e008402ac8f535e058d5e28a5c46b9ce9daa1
hs150521 <hs150521@163.com>
2026-02-11 14:55:13 +0800

    [Feat] SerialSubsystem with I2C

--- src/main/java/frc/robot/subsystems/SerialSubsystem/SerialSubsystem.java
+++ src/main/java/frc/robot/subsystems/SerialSubsystem/SerialSubsystem.java
@@ -1,79 +1,71 @@
 package frc.robot.subsystems.SerialSubsystem;
 
-import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.wpilibj.I2C;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import lombok.Getter;
 import org.littletonrobotics.junction.Logger;
 
 public class SerialSubsystem extends SubsystemBase {
+  private static final int I2C_ADDRESS = 0x08;
+  private static final double TIMEOUT_SEC = 0.5;
 
-    private SerialPort arduino;
-    private final StringBuilder rxBuffer = new StringBuilder();
+  private final I2C i2c;
 
-    @Getter private double speed = 0.0;
-    @Getter private String lastException = "";
-    private double lastRxTime = 0.0;
+  @Getter private double speed = 0.0;
+  @Getter private String lastException = "";
+  private double lastRxTime = 0.0;
 
-    private static final double TIMEOUT_SEC = 0.5;
+  public SerialSubsystem() {
+    I2C tmp;
+    try {
+      tmp = new I2C(I2C.Port.kMXP, I2C_ADDRESS);
+    } catch (Exception e) {
+      tmp = null;
+      lastException = e.getMessage();
+    }
+    i2c = tmp;
+  }
 
-    public SerialSubsystem() {
-        try {
-            arduino = new SerialPort(1000000, SerialPort.Port.kUSB);
-            arduino.setTimeout(0.0);
-            arduino.setReadBufferSize(256);
-        } catch (Exception e) {
-            arduino = null;
-        }
+  @Override
+  public void periodic() {
+    lastRxTime = Timer.getFPGATimestamp();
+
+    log();
+    if (i2c == null) return;
+
+    byte[] rx = new byte[4];
+    boolean aborted = i2c.readOnly(rx, 4);
+    if (aborted) {
+      lastException = "I2C read aborted";
+      return;
     }
 
-    @Override
-    public void periodic() {
-        if (arduino == null) return;
-
-        int available = arduino.getBytesReceived();
-        if (available <= 0) return;
-
-        byte[] data = arduino.read(available);
-        for (byte b : data) {
-            processByte((char) b);
-        }
+    float v;
+    try {
+      v = ByteBuffer.wrap(rx).order(ByteOrder.LITTLE_ENDIAN).getFloat();
+    } catch (Exception e) {
+      lastException = "Bad float";
+      return;
     }
 
-    private void processByte(char c) {
-        if (c == '<') {
-            rxBuffer.setLength(0);
-        } else if (c == '>') {
-            parseFrame(rxBuffer.toString().trim());
-            rxBuffer.setLength(0);
-        } else {
-            rxBuffer.append(c);
-        }
-    }
+    speed = v;
+    lastException = "";
+    lastRxTime = Timer.getFPGATimestamp();
 
-    private void parseFrame(String frame) {
-        lastRxTime = Timer.getFPGATimestamp();
+    log();
+  }
 
-        if (frame.startsWith("Speed(m/s):")) {
-            try {
-                speed = Double.parseDouble(frame.substring("Speed(m/s):".length()));
-                lastException = "";
-            } catch (Exception ignored) {
-            }
-            return;
-        }
+  public boolean isTimedOut() {
+    if (lastRxTime == 0) return true;
+    return Timer.getFPGATimestamp() - lastRxTime > TIMEOUT_SEC;
+  }
 
-        lastException = frame;
-    }
-
-    public boolean isTimedOut() {
-        if (lastRxTime == 0) return true;
-        return Timer.getFPGATimestamp() - lastRxTime > TIMEOUT_SEC;
-    }
-
-    public void log() {
-        Logger.recordOutput("Arduino/Speed", speed);
-        Logger.recordOutput("Arduino/Exception", lastException);
-        Logger.recordOutput("Arduino/TimedOut", isTimedOut());
-    }
+  public void log() {
+    Logger.recordOutput("Arduino/Speed", speed);
+    Logger.recordOutput("Arduino/Exception", lastException);
+    Logger.recordOutput("Arduino/TimedOut", isTimedOut());
+  }
 }
