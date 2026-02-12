cafa6e5bb7768c08755b4fbf0cfe47f2c122f425
Mai961 <michael96mai@gmail.com>
2026-02-12 21:32:55 +0800

    [fix]wholesystem tune

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -58,9 +58,9 @@ public class Robot extends LoggedRobot {
     public void robotPeriodic() {
         robotContainer.robotPeriodic();
         CommandScheduler.getInstance().run();
-        if (m_gcTimer.advanceIfElapsed(5)) {
-            System.gc();
-        }
+        // if (m_gcTimer.advanceIfElapsed(5)) {
+        //     System.gc();
+        // }
     }
 
     @Override
