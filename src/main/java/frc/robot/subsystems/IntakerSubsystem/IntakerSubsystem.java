8f948435d647c6907094b8f362291aec1f61e3f7
Mai961 <michael96mai@gmail.com>
2026-02-13 13:29:58 +0800

    wip:minorfix

--- src/main/java/frc/robot/subsystems/IntakerSubsystem/IntakerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakerSubsystem/IntakerSubsystem.java
@@ -2,8 +2,8 @@ package frc.robot.subsystems.IntakerSubsystem;
 
 import static edu.wpi.first.units.Units.Meters;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
-import static frc.robot.subsystems.Configs.IntakerExtensionConfig.IntakerExtensionParams.*;
-import static frc.robot.subsystems.Configs.IntakerRollerConfig.IntakerRollerParams.*;
+import static frc.robot.subsystems.Configs.IntakeConfig.IntakerExtensionParams.*;
+import static frc.robot.subsystems.Configs.IntakeConfig.IntakerRollerParams.*;
 
 import edu.wpi.first.units.measure.Distance;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -29,41 +29,5 @@ public class IntakerSubsystem {
         roller.runVelocity(RotationsPerSecond.of(idleVelRPS));
     }
 
-    public Command retractIntake() {
-        return Commands.parallel(
-                        roller.runVelocity(RotationsPerSecond.of(idleVelRPS))
-                                .withInterruptBehavior(Command.InterruptionBehavior.kCancelSelf),
-                        extension
-                                .runPosition(Meters.of(retractPosMeters))
-                                .withInterruptBehavior(Command.InterruptionBehavior.kCancelSelf))
-                .withInterruptBehavior(Command.InterruptionBehavior.kCancelSelf);
-    }
 
-    public Command outtake() {
-        return isDeployed()
-                ? roller.runVelocity(RotationsPerSecond.of(outtakeVelRPS))
-                        .withInterruptBehavior(Command.InterruptionBehavior.kCancelSelf)
-                : Commands.none();
-    }
-
-    public Command intake() {
-        return isDeployed()
-                ? roller.runVelocity(RotationsPerSecond.of(intakeVelRPS))
-                        .withInterruptBehavior(Command.InterruptionBehavior.kCancelSelf)
-                : Commands.none();
-    }
-
-    public Command deployIntake() {
-        return Commands.parallel(
-                        roller.runVelocity(RotationsPerSecond.of(intakeVelRPS))
-                                .withInterruptBehavior(Command.InterruptionBehavior.kCancelSelf),
-                        extension
-                                .runPosition(Meters.of(deployPosMeters))
-                                .withInterruptBehavior(Command.InterruptionBehavior.kCancelSelf))
-                .withInterruptBehavior(Command.InterruptionBehavior.kCancelSelf);
-    }
-
-    public boolean isDeployed() {
-        return extension.getCurrSetpoint().equals(Meters.of(deployPosMeters));
-    }
 }
