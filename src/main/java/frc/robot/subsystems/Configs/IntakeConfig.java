9b44bc467e12347cfce7814f01d2a258d84e3bb7
Mai961 <michael96mai@gmail.com>
2026-02-17 13:32:07 +0800

    [feat]intakefeed

--- src/main/java/frc/robot/subsystems/Configs/IntakeConfig.java
+++ src/main/java/frc/robot/subsystems/Configs/IntakeConfig.java
@@ -93,7 +93,11 @@ public class IntakeConfig {
         // Tolerances / behavior
         public static final double atGoalToleranceMeters = 0.01;
         public static final double deployPosMeters = 0.2;
+        public static final double feedPosMeters = 0.07;
         public static final double retractPosMeters = 0.0;
+
+        /** Oscillation rate (Hz) for runFeed: deploy <-> feed cycles per second */
+        public static final double feedOscillationRateHz = 0.5;
 
         public static final boolean isBrake = true;
     }
