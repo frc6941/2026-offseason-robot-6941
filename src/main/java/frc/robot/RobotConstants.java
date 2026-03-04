f2c5baa6b629054edcc12252c15a6ef8e2657b64
hs150521 <hs150521@163.com>
2026-03-04 19:24:00 +0800

    [Fix] revert is10541

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -10,21 +10,22 @@ import lib.ironpulse.utils.Logging;
  * using NTParameter annotation.
  */
 public final class RobotConstants {
-    public static final String RIOSerial10541 = "03415993";
     // Robot timing constants
     public static final double LOOPER_DT = 0.01; // 50Hz control loop
+    // CAN bus configuration
+    public static final String CANIVORE_CAN_BUS_NAME = "6941Canivore0";
     public static final String ROBORIO_CAN_BUS_NAME = "rio";
+    public static final CANBus CANIVORE_CAN_BUS = new CANBus(CANIVORE_CAN_BUS_NAME);
     public static final CANBus ROBORIO_CAN_BUS = new CANBus(ROBORIO_CAN_BUS_NAME);
     // Hardware device IDs
     public static final int PIGEON_ID = 14; // Pigeon2 IMU device ID
     public static final int LED_PORT = 0;
     public static final int LED_LENGTH = 30;
+    public static final String RIOSerial10541 = "03415993";
+    public static boolean disableHAL = false;
     public static boolean is10541 =
             RobotController.getSerialNumber().matches(RobotConstants.RIOSerial10541);
-    // CAN bus configuration
-    public static final String CANIVORE_CAN_BUS_NAME = is10541 ? "10541Canivore0" : "6941Canivore0";
-    public static final CANBus CANIVORE_CAN_BUS = new CANBus(CANIVORE_CAN_BUS_NAME);
-    public static boolean disableHAL = false;
+    ;
 
     // auto robot config
     public static RobotConfig AUTO_ROBOT_CONFIG;
