69683dcf9e28d887714526e79450746b91377abf
Mai961 <michael96mai@gmail.com>
2026-03-05 17:25:11 +0800

    [fix]good constants for final design

--- src/main/java/frc/robot/subsystems/Configs/ShotCalculatorConfig.java
+++ src/main/java/frc/robot/subsystems/Configs/ShotCalculatorConfig.java
@@ -19,8 +19,8 @@ public final class ShotCalculatorConfig {
         public static final double trajectoryBiasDegGOAL = -2;
         public static final double trajectoryBiasDegFEED = -2;
         public static final double loookfwdDistanceScale = 0;
-        public static final double lateralVelocityCompScale = 1;
-        public static final double lookfwdDelayCycles = 12;
+        public static final double lateralVelocityCompScale = 0.7;
+        public static final double lookfwdDelayCycles = 10;
         public static final double lookfwdFlightScale = 0;
 
         public static final double lookfwdMinCycles = 0.0;
@@ -28,7 +28,7 @@ public final class ShotCalculatorConfig {
 
         // Linear mapping (actuator-space)
         // rpm = rpmA * exitSpeed + rpmB * bbaDeg + rpmC
-        public static final double rpmA = 275;
+        public static final double rpmA = 257;
         public static final double rpmB = 0;
         public static final double rpmC = 150;
 
--- src/main/java/frc/robot/subsystems/Configs/SwerveMK5Config.java
+++ src/main/java/frc/robot/subsystems/Configs/SwerveMK5Config.java
@@ -42,12 +42,12 @@ public final class SwerveMK5Config {
                     .maxAngularVelocity(DegreesPerSecond.of(1000))
                     // accelerate in 0.32s, also must be smaller than the defined module limit to be
                     // actually effective
-                    .maxAngularAcceleration(DegreesPerSecondPerSecond.of(2200)) // 1000-1472
+                    .maxAngularAcceleration(DegreesPerSecondPerSecond.of(2500)) // 1000-1472
                     .build();
 
     public static SwerveModuleLimit kShootingSwerveLimit =
             SwerveModuleLimit.builder()
-                    .maxDriveVelocity(InchesPerSecond.of(2000.0 / 60.0 / 7.03 * Math.PI * 4.0))
+                    .maxDriveVelocity(InchesPerSecond.of(3000.0 / 60.0 / 7.03 * Math.PI * 4.0))
                     .maxDriveAcceleration(MetersPerSecondPerSecond.of(18))
                     // omega (rps) = 7368rpm (X44 with FOC) / 60 / (287/11) ~= 4.707 rps
                     .maxSteerAngularVelocity(RotationsPerSecond.of(7368.0 / 60.0 / (287.0 / 11.0)))
