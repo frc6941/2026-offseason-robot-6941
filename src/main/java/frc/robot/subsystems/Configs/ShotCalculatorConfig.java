dd877472c60eab2199cf89cf20fd4e7b8a4a1a07
hs150521 <hs150521@163.com>
2026-03-13 18:14:10 +0800

    Update at 'Fri Mar 13 18:14:10 HKT 2026'

--- src/main/java/frc/robot/subsystems/Configs/ShotCalculatorConfig.java
+++ src/main/java/frc/robot/subsystems/Configs/ShotCalculatorConfig.java
@@ -11,28 +11,28 @@ import lib.ntext.NTParameter;
  * cache is required.
  */
 public final class ShotCalculatorConfig {
-  private ShotCalculatorConfig() {}
+    private ShotCalculatorConfig() {}
 
-  @NTParameter(tableName = "Params/ShotCalculator")
-  public static final class ShotCalculatorParams {
-    // Model tuning (model-space adjustments)
-    public static final double trajectoryBiasDegGOAL = is10541 ? -6 : -6;
-    public static final double trajectoryBiasDegFEED = -2;
-    public static final double loookfwdDistanceScale = 0;
-    public static final double lateralVelocityCompScale = is10541 ? 1.01 : 1.2;
-    public static final double lookfwdDelayCycles = is10541 ? 1 : 3;
+    @NTParameter(tableName = "Params/ShotCalculator")
+    public static final class ShotCalculatorParams {
+        // Model tuning (model-space adjustments)
+        public static final double trajectoryBiasDegGOAL = is10541 ? -6 : -6;
+        public static final double trajectoryBiasDegFEED = -2;
+        public static final double loookfwdDistanceScale = 0;
+        public static final double lateralVelocityCompScale = is10541 ? 1.01 : 1.2;
+        public static final double lookfwdDelayCycles = is10541 ? 1 : 3;
 
-    public static final double lookfwdFlightScale = 0;
+        public static final double lookfwdFlightScale = 0;
 
-    public static final double lookfwdMinCycles = 0.0;
-    public static final double lookfwdMaxCycles = 20.0;
+        public static final double lookfwdMinCycles = 0.0;
+        public static final double lookfwdMaxCycles = 20.0;
 
-    // Linear mapping (actuator-space)
-    // rpm = rpmA * exitSpeed + rpmB * bbaDeg + rpmC
-    public static final double rpmA = is10541 ? 337 : 237.1;
-    public static final double rpmB = is10541 ? 0 : 0;
-    public static final double rpmC = is10541 ? -368.8 : 146;
+        // Linear mapping (actuator-space)
+        // rpm = rpmA * exitSpeed + rpmB * bbaDeg + rpmC
+        public static final double rpmA = is10541 ? 337 : 237.1;
+        public static final double rpmB = is10541 ? 0 : 0;
+        public static final double rpmC = is10541 ? -368.8 : 146;
 
-    public static final double distanceScaler = 1.03;
-  }
+        public static final double distanceScaler = 1.03;
+    }
 }
