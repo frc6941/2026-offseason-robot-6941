11d714541766cbae39cf0f9a4f04d9493673f3e4
hs150521 <hs150521@163.com>
2026-03-15 08:25:52 +0800

    Update at 'Sun Mar 15 08:25:51 HKT 2026'

--- src/main/java/frc/robot/auto/AutoFile.java
+++ src/main/java/frc/robot/auto/AutoFile.java
@@ -20,188 +20,172 @@ import org.json.simple.parser.ParseException;
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
 public class AutoFile {
-    private static final Map<String, PathPlannerPath> autoPaths = new HashMap<>();
+  private static final Map<String, PathPlannerPath> autoPaths = new HashMap<>();
 
-    @Getter
-    private static final LoggedDashboardChooser<AutoType> autoChooser =
-            new LoggedDashboardChooser<AutoType>("Auto Type Chooser");
+  @Getter
+  private static final LoggedDashboardChooser<AutoType> autoChooser =
+      new LoggedDashboardChooser<AutoType>("Auto Type Chooser");
 
-    private static final LoggedDashboardChooser<AutoSide> sideChooser =
-            new LoggedDashboardChooser<AutoSide>("Auto Side Chooser");
-    private static final LoggedDashboardChooser<SweepMode> sweepModeChooser =
-            new LoggedDashboardChooser<SweepMode>("Sweep Mode Chooser");
-    private static final LoggedDashboardChooser<EndBehaviour> endBehaviourChooser =
-            new LoggedDashboardChooser<EndBehaviour>("End Behaviour Chooser");
+  private static final LoggedDashboardChooser<AutoSide> sideChooser =
+      new LoggedDashboardChooser<AutoSide>("Auto Side Chooser");
+  private static final LoggedDashboardChooser<SweepMode> sweepModeChooser =
+      new LoggedDashboardChooser<SweepMode>("Sweep Mode Chooser");
+  private static final LoggedDashboardChooser<EndBehaviour> endBehaviourChooser =
+      new LoggedDashboardChooser<EndBehaviour>("End Behaviour Chooser");
 
-    private static final Alert competitionNotSelectedAlert =
-            new Alert("Competition auto is not running", Alert.AlertType.kWarning);
-    private static final Alert invalidConfigAlert =
-            new Alert(
-                    "Auto configuration is invalid, fallback to Command.None()",
-                    Alert.AlertType.kError);
-    private static final Alert nullConfigAlert =
-            new Alert(
-                    "Auto configuration is null, fallback to Command.None()",
-                    Alert.AlertType.kError);
+  private static final Alert competitionNotSelectedAlert =
+      new Alert("Competition auto is not running", Alert.AlertType.kWarning);
+  private static final Alert invalidConfigAlert =
+      new Alert(
+          "Auto configuration is invalid, fallback to Command.None()", Alert.AlertType.kError);
+  private static final Alert nullConfigAlert =
+      new Alert("Auto configuration is null, fallback to Command.None()", Alert.AlertType.kError);
 
-    private static <E extends Enum<E>> void initializeChooser(
-            LoggedDashboardChooser<E> chooser, E[] values, E defaultValue) {
-        chooser.onChange((value) -> verifyOption());
-        chooser.addDefaultOption(defaultValue.toString(), defaultValue);
-        for (E e : values) {
-            chooser.addOption(e.toString(), e);
-        }
+  private static <E extends Enum<E>> void initializeChooser(
+      LoggedDashboardChooser<E> chooser, E[] values, E defaultValue) {
+    chooser.onChange((value) -> verifyOption());
+    chooser.addDefaultOption(defaultValue.toString(), defaultValue);
+    for (E e : values) {
+      chooser.addOption(e.toString(), e);
+    }
+  }
+
+  public static void init() {
+    initializeAutoPaths();
+    initializeChooser(autoChooser, AutoType.values(), AutoType.COMPETITION);
+    initializeChooser(sideChooser, AutoSide.values(), AutoSide.RIGHT);
+    initializeChooser(endBehaviourChooser, EndBehaviour.values(), EndBehaviour.FUEL);
+    initializeChooser(sweepModeChooser, SweepMode.values(), SweepMode.LONG);
+  }
+
+  private static void initializeAutoPaths() {
+    File[] files = new File(Filesystem.getDeployDirectory(), "pathplanner/paths").listFiles();
+    assert files != null;
+    for (File file : files) {
+      try {
+        // path files without extension
+        PathPlannerPath path =
+            PathPlannerPath.fromPathFile(file.getName().replaceFirst("[.][^.]+$", ""));
+        autoPaths.put(path.name, path);
+      } catch (IOException | ParseException e) {
+        throw new IllegalArgumentException("Failed to parse path file: " + file.getName(), e);
+      }
+    }
+  }
+
+  private static PathPlannerPath getAutoPath(String path) {
+    assert autoPaths.containsKey(path);
+    return autoPaths.get(path);
+  }
+
+  public static Command buildTest() {
+    return AutoActions.drivePastSlope(false, true);
+  }
+
+  public static Command buildAuto() {
+
+    AutoType selected = autoChooser.get();
+    if (selected == null) {
+      invalidConfigAlert.set(true);
+      return Commands.none();
     }
 
-    public static void init() {
-        initializeAutoPaths();
-        initializeChooser(autoChooser, AutoType.values(), AutoType.COMPETITION);
-        initializeChooser(sideChooser, AutoSide.values(), AutoSide.RIGHT);
-        initializeChooser(endBehaviourChooser, EndBehaviour.values(), EndBehaviour.FUEL);
-        initializeChooser(sweepModeChooser, SweepMode.values(), SweepMode.LONG);
+    return switch (selected) {
+      case TEST -> buildTest();
+      case COMPETITION -> buildCompetition();
+      case SHOOT ->
+          Commands.defer(
+              () -> shootingSuperstructure.shootWhenReady(false), Collections.singleton(shooter));
+    };
+  }
+
+  private static void verifyOption() {
+    if (autoChooser.get() == null
+        || sweepModeChooser.get() == null
+        || endBehaviourChooser.get() == null
+        || sideChooser.get() == null) {
+      nullConfigAlert.set(true);
+      SmartDashboard.putBoolean("Auto/Verified", false);
+      return;
+    } else {
+      nullConfigAlert.set(false);
     }
 
-    private static void initializeAutoPaths() {
-        File[] files = new File(Filesystem.getDeployDirectory(), "pathplanner/paths").listFiles();
-        assert files != null;
-        for (File file : files) {
-            try {
-                // path files without extension
-                PathPlannerPath path =
-                        PathPlannerPath.fromPathFile(file.getName().replaceFirst("[.][^.]+$", ""));
-                autoPaths.put(path.name, path);
-            } catch (IOException | ParseException e) {
-                throw new IllegalArgumentException(
-                        "Failed to parse path file: " + file.getName(), e);
-            }
-        }
+    if (autoChooser.get() == AutoType.COMPETITION) {
+      competitionNotSelectedAlert.set(false);
+      invalidConfigAlert.set(
+          sweepModeChooser.get() == SweepMode.LONG
+              && endBehaviourChooser.get() != EndBehaviour.FUEL);
+    } else {
+      competitionNotSelectedAlert.set(true);
+      invalidConfigAlert.set(false);
     }
+    SmartDashboard.putBoolean(
+        "Auto/Verified", !invalidConfigAlert.get() && !competitionNotSelectedAlert.get());
+  }
 
-    private static PathPlannerPath getAutoPath(String path) {
-        assert autoPaths.containsKey(path);
-        return autoPaths.get(path);
-    }
-
-    public static Command buildTest() {
-        return AutoActions.drivePastSlope(false, true);
-    }
-
-    public static Command buildAuto() {
-
-        AutoType selected = autoChooser.get();
-        if (selected == null) {
-            invalidConfigAlert.set(true);
-            return Commands.none();
-        }
-
-        return switch (selected) {
-            case TEST -> buildTest();
-            case COMPETITION -> buildCompetition();
-            case SHOOT -> Commands.defer(
-                    () -> shootingSuperstructure.shootWhenReady(false),
-                    Collections.singleton(shooter));
+  private static Command buildCompetition() {
+    if (invalidConfigAlert.get()) return Commands.none();
+    boolean isLeft = sideChooser.get() == AutoSide.LEFT;
+    String sweepPathName =
+        switch (sweepModeChooser.get()) {
+          case FAST -> "quickSweepRight";
+          case LONG -> "longSweepRight";
+          case NORMAL -> "sweepRight";
         };
-    }
+    return Commands.parallel(
+            // shooterDefault(),
+            Commands.sequence(
+                // Sweep
+                Commands.deadline(
+                    drivePastSlope(isLeft, true),
+                    Commands.defer(AutoActions::zeroEverything, Collections.emptySet())),
+                Commands.deadline(followPathFile(sweepPathName, isLeft), intake()),
+                drivePastSlope(isLeft, false),
 
-    private static void verifyOption() {
-        if (autoChooser.get() == null
-                || sweepModeChooser.get() == null
-                || endBehaviourChooser.get() == null
-                || sideChooser.get() == null) {
-            nullConfigAlert.set(true);
-            SmartDashboard.putBoolean("Auto/Verified", false);
-            return;
-        } else {
-            nullConfigAlert.set(false);
-        }
+                // FUEL
+                Commands.parallel(
+                        shoot(),
+                        new ConditionalCommand(
+                            Commands.deadline(allignToDepot(), oscillateIntakeFeed()),
+                            Commands.deadline(allignToStation(), oscillateIntakeFeed()),
+                            () -> isLeft))
+                    .onlyIf(() -> endBehaviourChooser.get() == EndBehaviour.FUEL),
 
-        if (autoChooser.get() == AutoType.COMPETITION) {
-            competitionNotSelectedAlert.set(false);
-            invalidConfigAlert.set(
-                    sweepModeChooser.get() == SweepMode.LONG
-                            && endBehaviourChooser.get() != EndBehaviour.FUEL);
-        } else {
-            competitionNotSelectedAlert.set(true);
-            invalidConfigAlert.set(false);
-        }
-        SmartDashboard.putBoolean(
-                "Auto/Verified", !invalidConfigAlert.get() && !competitionNotSelectedAlert.get());
-    }
+                // CLIMB
+                Commands.sequence(
+                        Commands.deadline(
+                            allignToClimb(isLeft),
+                            Commands.parallel(
+                                oscillateIntakeFeed().withTimeout(20),
+                                climbUp(),
+                                shoot().withTimeout(20))),
+                        climbed().alongWith(shoot()))
+                    .onlyIf(() -> endBehaviourChooser.get() == EndBehaviour.CLIMB)),
+            Commands.parallel(intake.zeroCommand(), intake()))
+        .withInterruptBehavior(Command.InterruptionBehavior.kCancelIncoming);
+    // .beforeStarting(() -> swerve.removeDefaultCommand());
+  }
 
-    private static Command buildCompetition() {
-        if (invalidConfigAlert.get()) return Commands.none();
-        boolean isLeft = sideChooser.get() == AutoSide.LEFT;
-        String sweepPathName =
-                switch (sweepModeChooser.get()) {
-                    case FAST -> "quickSweepRight";
-                    case LONG -> "longSweepRight";
-                    case NORMAL -> "sweepRight";
-                };
-        return Commands.parallel(
-                        // shooterDefault(),
-                        Commands.sequence(
-                                // Sweep
-                                Commands.deadline(
-                                        drivePastSlope(isLeft, true),
-                                        Commands.defer(
-                                                AutoActions::zeroEverything,
-                                                Collections.emptySet())),
-                                Commands.deadline(followPathFile(sweepPathName, isLeft), intake()),
-                                drivePastSlope(isLeft, false),
+  private enum AutoType {
+    COMPETITION,
+    TEST,
+    SHOOT
+  }
 
-                                // FUEL
-                                Commands.parallel(
-                                                shoot(),
-                                                new ConditionalCommand(
-                                                        Commands.deadline(
-                                                                allignToDepot(),
-                                                                oscillateIntakeFeed()),
-                                                        Commands.deadline(
-                                                                allignToStation(),
-                                                                oscillateIntakeFeed()),
-                                                        () -> isLeft))
-                                        .onlyIf(
-                                                () ->
-                                                        endBehaviourChooser.get()
-                                                                == EndBehaviour.FUEL),
+  private enum AutoSide {
+    RIGHT,
+    LEFT
+  }
 
-                                // CLIMB
-                                Commands.sequence(
-                                                Commands.deadline(
-                                                        allignToClimb(isLeft),
-                                                        Commands.parallel(
-                                                                oscillateIntakeFeed()
-                                                                        .withTimeout(20),
-                                                                climbUp(),
-                                                                shoot().withTimeout(20))),
-                                                climbed().alongWith(shoot()))
-                                        .onlyIf(
-                                                () ->
-                                                        endBehaviourChooser.get()
-                                                                == EndBehaviour.CLIMB)))
-                .withInterruptBehavior(Command.InterruptionBehavior.kCancelIncoming);
-        // .beforeStarting(() -> swerve.removeDefaultCommand());
-    }
+  private enum SweepMode {
+    FAST,
+    LONG,
+    NORMAL
+  }
 
-    private enum AutoType {
-        COMPETITION,
-        TEST,
-        SHOOT
-    }
-
-    private enum AutoSide {
-        RIGHT,
-        LEFT
-    }
-
-    private enum SweepMode {
-        FAST,
-        LONG,
-        NORMAL
-    }
-
-    private enum EndBehaviour {
-        CLIMB,
-        FUEL
-    }
+  private enum EndBehaviour {
+    CLIMB,
+    FUEL
+  }
 }
