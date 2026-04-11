5458f34ab30a195927ed89922bec30ee362f4278
Minda Huang <69568611+hs150521@users.noreply.github.com>
2026-04-11 15:25:00 +0800

    [Fix] Turret Encoder Offset

--- src/main/java/frc/robot/subsystems/Configs/TurretConfig.java
+++ src/main/java/frc/robot/subsystems/Configs/TurretConfig.java
@@ -20,9 +20,9 @@ public class TurretConfig {
     public static final int TURRET_ENCODER_G1_ID = 59;
     public static final int TURRET_ENCODER_G2_ID = 58;
     public static final Angle TURRET_ENCODER_G1_OFFSET =
-            Rotations.of(is10541 ? -0.515869140625 : -0.370361328125);
+            Rotations.of(is10541 ? -0.545166015625 : -0.370361328125);
     public static final Angle TURRET_ENCODER_G2_OFFSET =
-            Rotations.of(is10541 ? -0.740966796875 : -0.990478515625); // -0.837890625
+            Rotations.of(is10541 ? -0.50439453125 : -0.990478515625); // -0.837890625
     // Zeroing Coder constants
     public static final int G0_TOOTH_COUNT = 88;
     public static final int G1_TOOTH_COUNT = 16;
