5de93ad21f430bfcc8848da288319430ecb88e58
LCsiu <yooyooyoojun@163.com>
2026-04-28 01:35:07 -0500

    minor params tuning for turret

--- src/main/java/frc/robot/subsystems/Configs/TurretConfig.java
+++ src/main/java/frc/robot/subsystems/Configs/TurretConfig.java
@@ -84,7 +84,7 @@ public class TurretConfig {
     @NTParameter(tableName = "Params/" + NAME + "Pos")
     public static final class TurretPosParams {
         // shold be small
-        public static final double kpSeek = 4;
+        public static final double kpSeek = 8;
         public static final double kiSeek = 0.0;
         public static final double kdSeek = 0.0;
 
@@ -99,6 +99,6 @@ public class TurretConfig {
         // Hysteresis thresholds for automatic mode switching.
         // Must satisfy seekEnterErrorDegrees > trackEnterErrorDegrees.
         public static final double seekEnterErrorDegrees = 25;
-        public static final double trackEnterErrorDegrees = 8;
+        public static final double trackEnterErrorDegrees = 4;
     }
 }
