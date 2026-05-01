7cc85e9f4a47c7dab48c760481e29ede677e7f3e
LCsiu <yooyooyoojun@163.com>
2026-05-01 12:24:31 -0500

    Update at 'Fri May 01 12:24:30 CDT 2026'

--- src/main/java/frc/robot/auto/AutoFile.java
+++ src/main/java/frc/robot/auto/AutoFile.java
@@ -129,13 +129,13 @@ public class AutoFile {
                     Commands.defer(
                             () -> shootingSuperstructure.shootWhenReady(false),
                             Set.of(shooter, spindexer)),
-                    buildMiddleDepot());
+                    Commands.sequence(new WaitCommand(3),buildMiddleDepot()));
 
             case MIDDLE_OUTPOST -> Commands.parallel(
                     Commands.defer(
                             () -> shootingSuperstructure.shootWhenReady(false),
                             Set.of(shooter, spindexer)),
-                    buildMiddleOutpost());
+                    Commands.sequence(new WaitCommand(3),buildMiddleOutpost()));
 
             case SHOOT -> Commands.defer(
                     () -> shootingSuperstructure.shootWhenReady(false), Set.of(shooter, spindexer));
